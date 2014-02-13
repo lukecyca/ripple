7ab8bc715c7892a0790cb7c1b3a7b92bfdf03e0d
Luke Cyca <me@lukecyca.com>
2014-02-13 11:35:11 -0800

    Support monitoring transactions from an aribtrary past ledger

--- ripple.go
+++ ripple.go
@@ -6,8 +6,10 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"math/big"
 	"strconv"
+	"time"
 )
 
 type Amount struct {
@@ -63,35 +65,49 @@ type Transaction struct {
 	TxnSignature    string
 }
 
-type Message struct {
-	LedgerHash  string `json:"ledger_hash"`
-	LedgerIndex int    `json:"ledger_index"`
-	Status      string
-	Transaction *Transaction
-	Type        string
-}
-
-func (m *Message) String() string {
-	if m.Type == "transaction" {
-		t := m.Transaction
-
-		if t.TransactionType == "Payment" {
-			return ("Payment " + t.Hash +
-				" from " + t.Account + " to " +
-				t.Destination + " " +
-				strconv.Itoa(t.DestinationTag) + " " +
-				t.Amount.Value.FloatString(5) + " " +
-				t.Amount.Currency)
-		}
-
-		return t.TransactionType + " " + t.Hash
+func (t *Transaction) String() string {
+	if t.TransactionType == "Payment" {
+		return ("Payment " + t.Hash +
+			" from " + t.Account + " to " +
+			t.Destination + " " +
+			strconv.Itoa(t.DestinationTag) + " " +
+			t.Amount.Value.FloatString(5) + " " +
+			t.Amount.Currency)
 	}
 
-	return m.Type
+	return t.TransactionType + " " + t.Hash
+}
+
+type Ledger struct {
+	Accepted     bool
+	CloseTime    uint64 `json:"close_time"`
+	Closed       bool
+	Hash         string
+	Index        string `json:"ledger_index"`
+	ParentHash   string
+	Transactions []Transaction
+}
+
+type Result struct {
+	Ledger *Ledger
+}
+
+type Message struct {
+	Id           int
+	Type         string
+	Result       *Result
+	Status       string
+	Error        string
+	LedgerHash   string `json:"ledger_hash"`
+	LedgerIndex  uint64 `json:"ledger_index"`
+	LedgerTime   uint64 `json:"ledger_time"`
+	Transaction  *Transaction
+	TxnCount     int    `json:"txn_count"`
+	ServerStatus string `json:"server_status"`
 }
 
 type Connection struct {
-	Messages chan *Message
+	Transactions chan *Transaction
 
 	conn *websocket.Conn
 }
@@ -105,7 +121,7 @@ func (r *Connection) Connect(uri string) (err error) {
 	}
 
 	// Subscribe to all transactions
-	msg := "{\"command\":\"subscribe\",\"id\":1,\"streams\":[\"transactions\"]}"
+	msg := "{\"command\":\"subscribe\",\"id\":1,\"streams\":[\"server\", \"ledger\", \"transactions\"]}"
 	err = websocket.Message.Send(r.conn, msg)
 	if err != nil {
 		return err
@@ -125,7 +141,31 @@ func (r *Connection) Connect(uri string) (err error) {
 	return
 }
 
-func (r *Connection) Monitor() {
+func (r *Connection) getPastLedgers(startLedger uint64, endLedger uint64) {
+	// Asks the server for each ledger between start and end, inclusive
+
+	if startLedger > endLedger {
+		panic("Invalid ledger range")
+	}
+	log.Printf("Getting past ledgers [%d, %d]\n", startLedger, endLedger)
+
+	for i := startLedger; i <= endLedger; i++ {
+		msg := fmt.Sprintf("{\"command\":\"ledger\",\"id\":2,\"ledger_index\":%d,\"transactions\":1,\"expand\":1}", i)
+		err := websocket.Message.Send(r.conn, msg)
+		if err != nil {
+			panic("Could not get ledger: " + err.Error())
+		}
+
+		time.Sleep(time.Second)
+	}
+
+	log.Printf("Completed ledgers [%d, %d]\n", startLedger, endLedger)
+}
+
+func (r *Connection) MonitorTransactionsFrom(startLedger uint64) {
+	currentLedger := startLedger - 1
+	currentLedgerTxnsLeft := 0
+
 	for {
 		var err error
 		var m Message
@@ -138,6 +178,48 @@ func (r *Connection) Monitor() {
 			fmt.Println("Could not receive: " + err.Error())
 			break
 		}
-		r.Messages <- &m
+
+		switch m.Type {
+		case "ledgerClosed":
+			if m.LedgerIndex > currentLedger+1 {
+				//fmt.Printf("Missing ledgers between %d and %d\n", currentLedger, m.LedgerIndex)
+				//panic("")
+				go r.getPastLedgers(currentLedger+1, m.LedgerIndex-1)
+			}
+			if currentLedgerTxnsLeft != 0 {
+				log.Printf("Missing %d transactions from ledger %d\n", currentLedgerTxnsLeft, currentLedger)
+				panic("")
+			}
+			fmt.Println(m.LedgerIndex)
+			currentLedger = m.LedgerIndex
+			currentLedgerTxnsLeft = m.TxnCount
+
+		case "transaction":
+			if m.LedgerIndex != currentLedger {
+				log.Printf("Received out-of-order transaction from ledger %d\n", m.LedgerIndex)
+				panic("")
+			}
+			currentLedgerTxnsLeft--
+			r.Transactions <- m.Transaction
+
+		case "serverStatus":
+			log.Printf("SERVER: %s\n", m.ServerStatus)
+
+		case "response":
+			if m.Id == 2 && m.Status == "success" {
+				fmt.Println(m.Result.Ledger.Index)
+				for _, t := range m.Result.Ledger.Transactions {
+					r.Transactions <- &t
+				}
+			} else {
+				log.Printf("Unknown response ID: %d, status: %s\n", m.Id, m.Status)
+			}
+
+		case "error":
+			log.Printf("Error: %s\n", m.Error)
+
+		default:
+			log.Printf("Unknown message type: %s\n", m.Type)
+		}
 	}
 }

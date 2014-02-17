bc7b1a9aad665473671c1f7d2260ef282d84ca4a
Luke Cyca <me@lukecyca.com>
2014-02-17 08:36:26 -0800

    Oganized source files

--- connection.go
+++ connection.go
@@ -2,102 +2,12 @@ package ripple
 
 import (
 	"code.google.com/p/go.net/websocket"
-	"encoding/json"
 	"fmt"
 	"launchpad.net/tomb"
 	"log"
-	"math/big"
 	"strconv"
 	"time"
 )
-
-type Amount struct {
-	Currency string
-	Issuer   string
-	Value    big.Rat
-}
-
-func (a *Amount) UnmarshalJSON(b []byte) (err error) {
-
-	// Try interpret as IOU
-	var m map[string]string
-	err = json.Unmarshal(b, &m)
-	if err == nil {
-		a.Currency = m["currency"]
-		_, success := a.Value.SetString(m["value"])
-		if !success {
-			return fmt.Errorf("Could not interpret value: %s", m["value"])
-		}
-		a.Issuer = m["issuer"]
-		return
-	}
-
-	// Try interpret as XRP in drips
-	var s string
-	err = json.Unmarshal(b, &s)
-	if err == nil {
-		dripValue, success := a.Value.SetString(s)
-		if !success {
-			return fmt.Errorf("Could not interpret value: %s", s)
-		}
-		a.Value.Quo(dripValue, big.NewRat(1000000, 1))
-		a.Currency = "XRP"
-		return
-	}
-
-	return fmt.Errorf("Could not unmarshal amount: %s", b)
-}
-
-type Transaction struct {
-	Account         string
-	Amount          Amount
-	Date            int
-	Destination     string
-	DestinationTag  int
-	Fee             string
-	Flags           int
-	Hash            string
-	SendMax         *Amount
-	Sequence        int
-	SingingPubKey   string
-	TransactionType string
-	TxnSignature    string
-}
-
-type Ledger struct {
-	Accepted     bool
-	CloseTime    uint64 `json:"close_time"`
-	Closed       bool
-	Hash         string
-	Index        string `json:"ledger_index"`
-	ParentHash   string
-	Transactions []*Transaction
-}
-
-type Info struct {
-	BuildVersion string `json:"build_version"`
-	HostID       string
-	Peers        int
-}
-
-type Result struct {
-	Ledger *Ledger
-	Info   *Info
-}
-
-type Message struct {
-	Id           int
-	Type         string
-	Result       *Result
-	Status       string
-	Error        string
-	LedgerHash   string `json:"ledger_hash"`
-	LedgerIndex  uint64 `json:"ledger_index"`
-	LedgerTime   uint64 `json:"ledger_time"`
-	Transaction  *Transaction
-	TxnCount     int    `json:"txn_count"`
-	ServerStatus string `json:"server_status"`
-}
 
 type Connection struct {
 	Ledgers               chan *Ledger
--- connection_test.go
+++ connection_test.go
@@ -1,88 +1,8 @@
 package ripple
 
 import (
-	"encoding/json"
 	"testing"
 )
-
-func TestAmountUnmarshallXRP(t *testing.T) {
-	var raw = []byte(`"200000000"`)
-	var a Amount
-
-	err := json.Unmarshal(raw, &a)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if a.Currency != "XRP" || a.Value.FloatString(6) != "200.000000" {
-		t.Errorf("Expected 200 XRP, got %+v", a)
-	}
-}
-
-func TestAmountUnmarshallIOU(t *testing.T) {
-	var raw = []byte(`{
-		"currency": "BTC",
-		"issuer": "r000000000000000000000000000000000",
-		"value": "0.0001"
-    }`)
-	var a Amount
-
-	err := json.Unmarshal(raw, &a)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if a.Currency != "BTC" || a.Value.FloatString(6) != "0.000100" {
-		t.Errorf("Expected 0.0001 BTC, got %+v", a)
-	}
-
-	if a.Issuer != "r000000000000000000000000000000000" {
-		t.Errorf("Expected issuer r000000000000000000000000000000000, got %+v", a.Issuer)
-	}
-}
-
-func TestTransactionUnmarshall(t *testing.T) {
-	var raw = []byte(`{
-		"Account": "r00000000000000000000000000000000",
-		"Amount": {
-			"currency": "ICE",
-			"issuer": "r4H3F9dDaYPFwbrUsusvNAHLz2sEZk4wE5",
-			"value": "1"
-		},
-		"Destination": "r111111111111111111111111111111111",
-		"Fee": "15",
-		"Flags": 0,
-		"SendMax": {
-			"currency": "ICE",
-			"issuer": "r4H3F9dDaYPFwbrUsusvNAHLz2sEZk4wE5",
-			"value": "1.0015"
-		},
-		"Sequence": 42584,
-		"SigningPubKey": "03F3B30EF0F9DD92A6DA915B17767CB76FFE968A0D708131996BADA0C74E61DE48",
-		"TransactionType": "Payment",
-		"TxnSignature": "30450221008886E009E4E8A05CEF8E001380994A8FB6E28A3138E4AE593D7A3083964C52A9022057823FB88BF24E57B538BA3B78FBB3FE837F17C3F64E9B1E1A203C2A4D6AA75F",
-		"date": 444683680,
-		"hash": "4655982C1FD13073F8B4715B6D1485030F4BB3DA9201EF1DBE4D1834764A09BF"
-	}`)
-	var r Transaction
-
-	err := json.Unmarshal(raw, &r)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if r.Account != "r00000000000000000000000000000000" {
-		t.Errorf("Expected Account r000000000000000000000000000000000, got %+v", r.Account)
-	}
-
-	if r.Destination != "r111111111111111111111111111111111" {
-		t.Errorf("Expected Destination r111111111111111111111111111111111, got %+v", r.Destination)
-	}
-
-	if r.DestinationTag != 0 {
-		t.Errorf("Expected DestinationTag 0, got %+v", r.DestinationTag)
-	}
-}
 
 func TestHandleStreamMessages(t *testing.T) {
 	c := Connection{Ledgers: make(chan *Ledger, 1)}

6bb2c217fdcd222d2e1f9ac884ff9a1110ecbd6f
Luke Cyca <me@lukecyca.com>
2014-04-12 08:15:36 -0700

    Update public rippled servers

--- monitor.go
+++ monitor.go
@@ -11,8 +11,8 @@ import (
 const Timeout = time.Minute
 
 var URIs []string = []string{
-	"wss://s_west.ripple.com:443",
-	"wss://s_east.ripple.com:443",
+	"wss://s-west.ripple.com:443",
+	"wss://s-east.ripple.com:443",
 	"wss://s1.ripple.com:443",
 }
 
